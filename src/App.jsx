9b214615d010bf2bdf08e2098f677efc942814fa
Seulmin Ryu <seulminryu@mac.myfiosgateway.com>
2025-07-29 00:36:39 -0400

    player fetching updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Drafted from "./components/Drafted";
+import rankings from "./data/rankings.json";
+import playerInfo from "./data/playerInfo.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let main = rankings.map((player) => {
+    let name = player["PLAYER NAME"];
+    let age = 0;
+    for (const info of playerInfo) {
+      if (
+        info["player"].replace(/ /g, "").toLowerCase() ===
+        name.replace(/ /g, "").toLowerCase()
+      ) {
+        age = Number(info["age"]);
+        break;
+      }
+    }
+    return {
+      tier: Number(player["TIERS"]),
+      name: player["PLAYER NAME"],
+      team: player["TEAM"],
+      pos: player["POS"].slice(0, 2),
+      age: age,
+      bye: Number(player["BYE WEEK"]),
+    };
+  });
+
+  const [players, setPlayers] = useState(main);
+  const [borisChen, setBorisChen] = useState(false);
+  const [under28, setUnder28] = useState(false);
+
+  useEffect(() => {
+    if (borisChen) {
+      fetch("https://jayzheng-ff-api.herokuapp.com/rankings?format=half_ppr")
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Bad response");
+          }
+          return res.json();
+        })
+        .then((response) => {
+          const rankings = response["rankings"];
+          rankings.map((player) => {
+            let age = 0;
+            for (let info of playerInfo) {
+              if (
+                info["player"].replace(/ /g, "").toLowerCase() ===
+                player["name"].replace(/ /g, "").toLowerCase()
+              ) {
+                age = Number(info["age"]);
+                break;
+              }
+            }
+            return {
+              ...rankings,
+              age,
+            };
+          });
+          setPlayers(rankings);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [borisChen]);
+
+  useEffect(() => {
+    if (under28) {
+      const filtered = main.filter((player) => player.age < 28)
+      setPlayers(filtered)
+    }
+    else {
+      setPlayers(main)
+    }
+  }, [under28]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav className="flex">
+        <div className="m-4">
+          <input
+            type="checkbox"
+            checked={under28}
+            onChange={(e) => setUnder28(e.target.checked)}
+            name="under28"
+            id=""
+          />
+          <label className="ml-2">Under 28</label>
+        </div>
+        <div className="m-4">
+          <input
+            type="checkbox"
+            checked={borisChen}
+            onChange={(e) => setBorisChen(e.target.checked)}
+            name="borisChen"
+            id=""
+          />
+          <label className="ml-2">Boris Chen Tiers</label>
+        </div>
+      </nav>
+      <main className="flex justify-between">
+        <section className="w-1/3">
+          <Drafted />
+        </section>
+        <section className="w-2/3 grid grid-cols-2 grid-rows-2 gap-4">
+          <table></table>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Drafted.jsx
@@ -0,0 +1,24 @@
+export default function Drafted() {
+  return (
+    <table className="bg-gray-50 rounded-md">
+      <thead>
+        <tr className="bg-red-50">
+          <th className="p-4">Tier</th>
+          <th className="p-4">Name</th>
+          <th className="p-4">Team</th>
+          <th className="p-4">POS</th>
+          <th className="p-4">Age</th>
+          <th className="p-4">Year</th>
+          <th className="p-4">BYE</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="p-2">Chris</td>
+          <td className="p-2">HTML tables</td>
+          <td className="p-2">22</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
